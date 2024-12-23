67311435049840bacb04f3c963fa8ebe704448d1
matveev <klass-95e@mail.ru>
2024-12-23 16:44:06 +0300

    Added example with pattern Adapter

---
+++ src/com/company/patterns/adapter/ListProcessor.java
@@ -0,0 +1,7 @@
+package com.company.patterns.adapter;
+
+import java.util.List;
+
+public interface ListProcessor { //интерфейс в котором хотим работать с списоком
+    int sumList(List<Integer> list);
+}
